d15f9906b98a982e1ae7ba122d160c367e6508e3
Suman Mondal <suman.mondal@xconics.com>
2026-06-10 16:11:30 +0530

    eas setup

--- app.config.js
+++ app.config.js
@@ -7,6 +7,9 @@ module.exports = {
     extra: {
       posthogProjectToken: process.env.POSTHOG_PROJECT_TOKEN,
       posthogHost: process.env.POSTHOG_HOST,
+      "eas": {
+        "projectId": "442f377d-b5f1-4397-a8de-9009039a3f7b"
+      }
     },
   },
 }
